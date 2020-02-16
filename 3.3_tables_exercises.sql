59f2ec40414239e7a60edf1b9dad13ccf8a6cabe
Wenliang Sun <wenliangsonny@gmail.com>
2020-02-16 00:17:05 -0600

    updated

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,12 @@
+USE employees;
+
+SHOW TABLES;
+
+DESCRIBE employees; -- int, date, varchar, enum, date
+
+-- 6. dept_emp, dept_manager, employees, employees_with_departments, salaries, titles
+-- 7. all tables except salaries
+-- 8 dept_emp, dept_manager, employees, employees_with_departments, salaries, titles
+-- 9 no relationships 
+
+SHOW CREATE TABLE dept_manager; 
